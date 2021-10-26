e8aa7e8c8c5c70527cbbb13dd6251f01b34af76e
tshirt519 <tshirt519@gmail.com>
2021-10-26 14:22:57 +0900

    ✨ rakuten book API　検索アプリ

--- app/views.py
+++ app/views.py
@@ -4,7 +4,13 @@ from .forms import SearchForm
 import json
 import requests
 
-SEARCH_URL = 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&applicationId='
+SEARCH_URL = 'https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&applicationId=1069764878792879197'
+
+def get_api_data(params):
+  api = requests.get(SEARCH_URL, params=params).text
+  result = json.loads(api)
+  items = result['Items']
+  return items
 
 class IndexView(View):
   def get(self, request, *args, **kwargs):
@@ -12,5 +18,40 @@ class IndexView(View):
 
     return render(request, 'app/index.html', {
       'form': form,
-
     })
+
+  def post(self, request, *args, **kwargs):
+    form = SearchForm(request.POST or None)
+
+    if form.is_valid():
+      keyword = form.cleaned_data['title']
+      params = {
+        'title': keyword,
+        'hits': 28,
+      }
+      items = get_api_data(params)
+      book_data = []
+      for i in items:
+        item = i['Item']
+        title = item['title']
+        image = item['largeImageUrl']
+        isbn = item['isbn']
+        url = item['itemUrl']
+        query = {
+          'title': title,
+          'image': image,
+          'isbn': isbn,
+          'url': url,
+        }
+        book_data.append(query)
+        
+      return render(request, 'app/book.html', {
+        'book_data': book_data,
+        'keyword': keyword
+      })
+
+    return render(request, 'app/index.html', {
+      'form': form,
+  })
+
+
